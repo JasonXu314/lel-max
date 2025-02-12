f7049f4038f564921a012d5faec5bbdd8d285a4b
Jason Xu <Xinwen31415@gmail.com>
2025-02-12 11:22:10 -0600

    fully fixed sizing bug

--- src/client/lib/editor/blocks/utils/utils.ts
+++ src/client/lib/editor/blocks/utils/utils.ts
@@ -1,9 +1,13 @@
 import type { Block } from '../../Block';
-import { ChainBranchBlock } from '../classes';
+import { ChainBranchBlock, SlottableBlock } from '../classes';
 import { IfBlock } from '../control/If.block';
 
 export function effectiveHeight(height: number, block: Block, prune: (arg: number) => number): number {
-	return height === 0 ? block.height : block instanceof ChainBranchBlock && block.parent?.encapsulates(block) ? prune(height) : height + block.height;
+	return height === 0
+		? block.height
+		: (block instanceof ChainBranchBlock && block.parent?.encapsulates(block)) || (block instanceof SlottableBlock && block.host.encapsulates(block)) // TODO: consider if slottable blocks will 100% be encapsulated? (think except for ringed blocks?)
+		? prune(height)
+		: height + block.height;
 }
 
 export function hasIfBlock(result: boolean, block: Block, prune: (arg: boolean) => boolean): boolean {
