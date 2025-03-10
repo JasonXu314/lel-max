3058712d54391b84031be8d2790138ab75dfc093
Jason Xu <Xinwen31415@gmail.com>
2025-03-10 14:47:54 -0500

    disallow arbitrary path traversal

--- src/app.dtos.ts
+++ src/app.dtos.ts
@@ -1,12 +1,15 @@
-import { IsObject, IsString } from 'class-validator';
+import { IsObject, IsString, NotContains } from 'class-validator';
 import { fi } from './utils/utils';
 
 export type DirObject = { [segment: string]: string | DirObject };
 
 export class CreateBundleDTO {
 	@IsString({ each: true })
+	@NotContains('/', { each: true })
+	@NotContains('..', { each: true })
 	includes: string[] = fi();
 
 	@IsObject()
 	sources: any = fi();
 }
+
